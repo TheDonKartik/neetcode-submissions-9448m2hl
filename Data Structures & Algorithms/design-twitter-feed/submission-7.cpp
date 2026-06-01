ff6b554f0a873ce1a8c34293905b8c3dc553b610
TheDonKartik <150947603+TheDonKartik@users.noreply.github.com>
2026-06-01 23:52:50 +0530

    Add: design-twitter-feed - submission-7

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-7.cpp
@@ -0,0 +1,36 @@
+class Twitter {
+public:
+    unordered_map<int, set<int>> relation;
+    unordered_map<int, vector<pair<int,int>>> tweets;
+    int time = 0;
+    Twitter() {
+        
+    }
+    
+    void postTweet(int userId, int tweetId){
+        tweets[userId].push_back({time++,tweetId});
+    }
+    
+    vector<int> getNewsFeed(int userId) {
+        vector<pair<int,int>> feed = tweets[userId];
+        vector<int> res;
+        for (int i : relation[userId]){
+            feed.insert(feed.end(), tweets[i].begin(), tweets[i].end());
+        }
+        sort(feed.rbegin(), feed.rend());
+        if (feed.size() > 10) feed.erase(feed.begin() + 10, feed.end());
+        for (pair<int,int> i : feed){
+            res.push_back(i.second);
+        }
+        return res;
+    }
+    
+    void follow(int followerId, int followeeId) {
+        if (followerId != followeeId)
+        relation[followerId].insert(followeeId); 
+    }
+    
+    void unfollow(int followerId, int followeeId) {
+        relation[followerId].erase(followeeId);
+    }
+};
