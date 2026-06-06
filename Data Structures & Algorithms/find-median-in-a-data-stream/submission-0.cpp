e136775eedb7cd141ac770787d7876580e56c236
TheDonKartik <150947603+TheDonKartik@users.noreply.github.com>
2026-06-06 12:46:46 +0530

    Add: find-median-in-a-data-stream - submission-0

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-0.cpp
@@ -0,0 +1,18 @@
+class MedianFinder {
+public:
+    vector<double> v;
+    MedianFinder() {
+        
+    }
+    
+    void addNum(int num) {
+        v.push_back(num);
+        sort(v.begin(),v.end());
+    }
+    
+    double findMedian() {
+        int s = v.size();
+        if (s%2==0) return (v[s/2 -1] + v[(s/2)])/2;
+        else return v[s/2];
+    }
+};
