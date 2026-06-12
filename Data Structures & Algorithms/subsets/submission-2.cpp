7b88f7686c080ea8b57b91d0c5ef431cf61ed344
TheDonKartik <150947603+TheDonKartik@users.noreply.github.com>
2026-06-12 22:17:41 +0530

    Add: subsets - submission-2

---
+++ Data Structures & Algorithms/subsets/submission-2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> res = {{}};
+
+        for (int num : nums){
+            int size = res.size();
+            for (int j = 0; j < size; j++){
+                vector<int> temp = res[j];
+                temp.push_back(num);
+                res.push_back(temp);
+            }
+        }
+
+        return res;
+    }
+};
+ 
